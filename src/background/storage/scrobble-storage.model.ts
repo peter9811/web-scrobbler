531f7e486cf132cdc01c966b5f8d02d0ebb4c0e1
alexesprit <alex.esprit@gmail.com>
2020-07-27 19:34:24 +0300

    Rework core and tests
    Rewrite core and tests in TypeScript.
    Update module structure.

---
+++ src/background/storage/scrobble-storage.model.ts
@@ -0,0 +1,22 @@
+import CustomStorage from '@/background/storage/custom-storage';
+
+import { SongInfo } from '@/background/object/song';
+
+export default abstract class ScrobbleStorageModel extends CustomStorage {
+	/* Public methods */
+
+	async addSong(songInfo: SongInfo, scrobblerIds: string[]): Promise<void> {
+		const storageData = await this.getData();
+		const entryId = ScrobbleStorageModel.generateEntryId();
+
+		storageData[entryId] = { scrobblerIds, songInfo };
+		await this.saveData(storageData);
+	}
+
+	/* Static methods */
+
+	static generateEntryId(): string {
+		const uniqueNumber = Date.now();
+		return `id-${uniqueNumber}`;
+	}
+}
